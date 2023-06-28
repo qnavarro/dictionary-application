f0c9ae8d0b7b20aa89cc41410571df96cd70014a
Queenie <queenieb8me@gmail.com>
2023-06-28 14:13:41 -0700

    Completed primary set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<body>
+				<header className="App-header">Dictionary App</header>
+				<img src={logo} className="background" alt="logo" />
+			</body>
+		</div>
+	);
+}
+
+export default App;
