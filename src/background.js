3ecebea97bf233c582968eefe2b3b1df651b9339
Queenie <queenieb8me@gmail.com>
2023-06-29 10:49:30 -0700

    Update background.js

--- src/background.js
+++ src/background.js
@@ -3,7 +3,12 @@ import background from "./images/background.jpg";
 
 export default function Background() {
 	return (
-		<div style={{ backgroundImage: `url('${background}')` }}>
+		<div
+			style={{
+				backgroundImage: `url('${background}')`,
+				backgroundSize: "cover",
+			}}
+		>
 			Dictionary App{" "}
 		</div>
 	);
